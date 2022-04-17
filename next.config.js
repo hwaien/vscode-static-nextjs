ec262c0a61a53255cf0df8a5d3d9deddf9c9f4dc
Hwai-En Ho <dev@hwaien.com>
2022-04-17 05:59:42 +0000

    automate SSH key gen

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 // What is this file? https://nextjs.org/docs/api-reference/next.config.js/introduction
 
-/* eslint-disable node/no-unpublished-require */ // It is OK to ignore unpublished require because this file itself is also unpublished (only run at dev-time)
-
 // Adding support for bundle treemap visualization.
 // https://www.npmjs.com/package/@next/bundle-analyzer
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
